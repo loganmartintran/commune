3b0a3e747b82b62cc1447745f501057d43237f0f
Logan Tran <loganmartintran@icloud.com>
2017-04-22 13:13:46 -0400

    fix specs t test for the proper things

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -4,9 +4,8 @@ RSpec.describe Answer, type: :model do
   let(:question) { Question.create!(title: "New Post Title", body: "New Question Body", resolved: boolean) }
   let(:answer) { Answer.create!(body: "Answer Body", question: question) }
 
-  describe "attributes" do
-    it "has a body attribute" do
-      expect(answer).to have_attributes(body: "Answer Body")
+  it "responds to body" do
+      expect(answer).to respond_to(:body)
     end
   end
 
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -2,9 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Question, type: :model do
   let(:question) { Question.create!(title: "New Post Title", body: "New Question Body", resolved: boolean) }
+
   describe "attributes" do
-    it "has title and body and resolved attributes" do
-    expect(question).to have_attributes(title: "New Post Title", body: "New Question Body", resolved: boolean)
+    it "responds to title" do
+      expect(question).to respond_to(:title)
+    end
+
+    it "responds to body" do
+      expect(question).to respond_to(:body)
+    end
+
+    it "responds to resolved" do
+      expect(question).to respond_to(:resolved)
     end
   end
 
