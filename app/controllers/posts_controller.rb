017409cc574716382d95b9e91987945f0b494a13
Logan Tran <loganmartintran@icloud.com>
2017-05-03 01:03:21 -0500

    complete cp18_assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each_with_index do |post,index|
+      if index % 5 == 0
+        post.title = "SPAM"
+      end
+    end
   end
 
   def show
