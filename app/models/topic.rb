59b21067e98e1b0f4927874cd78b04306185a356
Logan Tran <loganmartintran@icloud.com>
2017-05-11 20:09:46 -0500

    complete cp22 without using form_group_tag

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,7 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_many :sponsored_posts
+
+  validates :name, length: {minimum: 5}, presence: true
+  validates :description, length: {minimum: 15}, presence: true
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -8,6 +8,12 @@ RSpec.describe Topic, type: :model do
 
   it { is_expected.to have_many(:posts) }
 
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:description) }
+
+  it { is_expected.to validate_length_of(:name).is_at_least(5) }
+  it { is_expected.to validate_length_of(:description).is_at_least(15) }
+
   describe "attributes" do
     it "has name, description, and public attributes" do
       expect(topic).to have_attributes(name: name, description: description, public: public)
