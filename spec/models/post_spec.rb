4687bcdb5a3158ec50abfd97b8d1f5f9b5bb7b16
Logan Tran <loganmartintran@icloud.com>
2017-05-11 19:29:49 -0500

    complete cp22

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,9 +5,17 @@ RSpec.describe Post, type: :model do
   let(:description) { RandomData.random_paragraph }
   let(:title) { RandomData.random_sentence }
   let(:body) {RandomData.random_paragraph }
-
   let(:topic) { Topic.create!(name: name, description: description) }
   let(:post) { topic.posts.create!(title: title, body: body) }
+
+  it { is_expected.to belong_to(:topic) }
+
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:body) }
+  it { is_expected.to validate_presence_of(:topic) }
+
+  it { is_expected.to validate_length_of(:title).is_at_least(5) }
+  it { is_expected.to validate_length_of(:body).is_at_least(20) }
 
   describe "attributes" do
     it "has title and body attributes" do
