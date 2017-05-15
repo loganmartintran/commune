30c8f7dc85b22973678f57ef0a518183c96ec420
Logan Tran <loganmartintran@icloud.com>
2017-05-15 00:42:21 -0500

    complete cp25 assignment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
 
     content_tag :div, capture(&block), class: css_class
   end
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+  end
 end
