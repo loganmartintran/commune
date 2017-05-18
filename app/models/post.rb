7f7310c97a478b5a07c4fbfc43593081bc91253c
Logan Tran <loganmartintran@icloud.com>
2017-05-17 21:29:39 -0500

    complete cp27 assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
   default_scope { order('created_at DESC') }
+  scope :ordered_by_title, -> { order('title') }
+  scope :ordered_by_reverse_created_at, -> { order('created_at').reverse_order }
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
