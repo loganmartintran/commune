29385ab58bbdd1e50323c20d0ba90bc093b5cc51
Logan Tran <loganmartintran@icloud.com>
2017-06-02 14:16:14 -0500

    complete cp30 assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
+  after_create :create_vote
 
   default_scope { order('rank DESC') }
   scope :ordered_by_title, -> { order('title') }
@@ -29,5 +30,11 @@ class Post < ActiveRecord::Base
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -73,4 +73,20 @@ RSpec.describe Post, type: :model do
       end
     end
   end
+
+  describe "#create_vote" do
+    it "sets the post up_votes to 1" do
+      expect(post.up_votes).to eq(1)
+    end
+
+    it "calls #create_vote when a post is created" do
+      post = topic.posts.new(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
+      expect(post).to receive(:create_vote).at_least(:once)
+      post.save!
+    end
+
+    it "associates the vote with the user of the post" do
+      expect(post.votes.first.user).to eq(post.user)
+    end
+  end
 end
