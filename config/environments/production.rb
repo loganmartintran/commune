4ff57d38071a23c192726ac3084bc960a999c658
Logan Tran <loganmartintran@icloud.com>
2017-06-05 12:26:19 -0500

    fix type in production.rb env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.defualt_url_options = { host: 'lit-taiga-67793.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'lit-taiga-67793.herokuapp.com' }
 end
